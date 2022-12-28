338f791bedc4404eb42f4b9ba178da58750bc044
Roman <vladimirovrm@gmail.com>
2022-12-28 11:25:20 +0300

    create directory component

--- src/App.js
+++ src/App.js
@@ -1,38 +1,9 @@
-const App = () => {
-	const categories = [
-		{
-			title: 'Hats',
-			id: 1,
-		},
-		{
-			title: 'Jackets',
-			id: 2,
-		},
-		{
-			title: 'Sneakers',
-			id: 1,
-		},
-		{
-			title: 'Womens',
-			id: 1,
-		},
-		{
-			title: 'Mans',
-			id: 1,
-		},
-	];
+import CatalogItem from "./components/directory/directory";
 
+const App = () => {
 	return (
-		<div className='categories-container'>
-			{categories.map(({ title }) => (
-				<div className='category-container'>
-					<div className='background-image' />
-					<div className='category-body-container'>
-						<h2>{title}</h2>
-						<p>Shop now</p>
-					</div>
-				</div>
-			))}
+		<div>
+			<CatalogItem />
 		</div>
 	);
 };
