5f6ea77e2c481b1c702646586a151e17a5a31912
Roman <vladimirovrm@gmail.com>
2022-12-29 19:46:12 +0300

    add icon

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
 
 import Home from './routes/home/home.jsx';
+import Navigation from './routes/navigation/navigation';
 
 const App = () => {
 	return (
 		<Routes>
-			<Route pat='/' element={Home} />
+			<Route path='/' element={<Navigation />}>
+				<Route index element={<Home />} />
+			</Route>
 		</Routes>
 	);
 };
