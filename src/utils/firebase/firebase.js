48eb74c1a68de7d680fbf82a3b223206af570806
Roman <vladimirovrm@gmail.com>
2023-01-01 16:08:08 +0300

    add env

--- src/utils/firebase/firebase.js
+++ src/utils/firebase/firebase.js
@@ -10,15 +10,14 @@ import {
 import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
 
 const firebaseConfig = {
-	apiKey: 'AIzaSyCxJUwVxFi1X4X_g1SlWVHDHtRB97qLXhw',
-	authDomain: 'crown-7165a.firebaseapp.com',
-	projectId: 'crown-7165a',
-	storageBucket: 'crown-7165a.appspot.com',
-	messagingSenderId: '917613125662',
-	appId: '1:917613125662:web:b05a397efa11bff87d4e0f',
+	apiKey: process.env.REACT_APP_API_KEY,
+	authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+	projectId: process.env.REACT_APP_PROJECT_ID,
+	storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+	messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+	appId: process.env.REACT_APP_APP_ID,
 };
 
-// Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
 
 const googleProvider = new GoogleAuthProvider();
