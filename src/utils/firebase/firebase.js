080f1c610ff53544ba6f8eda124b2e9cddb443c2
Roman <vladimirovrm@gmail.com>
2023-01-02 20:13:52 +0300

    change firebase

--- src/utils/firebase/firebase.js
+++ src/utils/firebase/firebase.js
@@ -5,6 +5,7 @@ import {
 	signInWithPopup,
 	GoogleAuthProvider,
 	createUserWithEmailAndPassword,
+	signInWithEmailAndPassword,
 } from 'firebase/auth';
 
 import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
@@ -60,7 +61,7 @@ export const createUserDocumentFromAuth = async (
 				...addIditionalInformation,
 			});
 		} catch (error) {
-			console.log('error', error.message);
+			console.error('error', error.message);
 		}
 	}
 
@@ -73,4 +74,12 @@ export const createAuthUserWithEmailAndPassword = async (email, password) => {
 	}
 
 	return await createUserWithEmailAndPassword(auth, email, password);
+};
+
+export const signInAuthUserWithEmailAndPassword = async (email, password) => {
+	if (!email || !password) {
+		return;
+	}
+
+	return await signInWithEmailAndPassword(auth, email, password);
 };
