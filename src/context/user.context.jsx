6db30945c95a5d821ffd1cb8081bb17832691f84
Roman <vladimirovrm@gmail.com>
2023-01-04 15:21:18 +0300

    update context

--- src/context/user.context.jsx
+++ src/context/user.context.jsx
@@ -1,13 +1,30 @@
-import { createContext, useState } from 'react';
+import { createContext, useState, useEffect } from 'react';
+
+import {
+	onAuthStateChangedListener,
+	createUserDocumentFromAuth,
+} from '../utils/firebase/firebase';
 
 export const UserContext = createContext({
-	currentUser: null,
 	setCurrentUser: () => null,
+	currentUser: null,
 });
 
 export const UserProvider = ({ children }) => {
 	const [currentUser, setCurrentUser] = useState(null);
 	const value = { currentUser, setCurrentUser };
+
+	useEffect(() => {
+		const unSubscribe = onAuthStateChangedListener((user) => {
+			if (user) {
+				createUserDocumentFromAuth(user);
+			}
+			setCurrentUser(user);
+			console.log(user);
+		});
+
+		return unSubscribe;
+	}, []);
 
 	return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
 };
