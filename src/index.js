9dfa97ad82dffc42d08b8f91de7d138ee59ecb62
Roman <vladimirovrm@gmail.com>
2023-01-04 20:20:32 +0300

    adding cart context and shhoping icon as well as cart dropdown

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { UserProvider } from './context/user.context';
 import { ProductsProvider } from './context/product.context';
-import { CartProvider } from './context/product.context';
+import { CartProvider } from './context/cart.context';
 
 import './index.scss';
 
